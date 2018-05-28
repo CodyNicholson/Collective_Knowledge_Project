d5db1d79f08486463c43288c01f5aeecce7035a4
Brandon Martin <bsm2112@yahoo.com>
2018-05-28 17:48:08 -0500

    Sockets are working

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ io.on('connection', (client) => {
     // Data will be the text user is entering
     console.log('SERVER: EDITING EVENT')
     console.log(data)
-    //client.broadcast.to(data.room).emit('change text', data.text);
+    client.broadcast.to(data.article).emit('change text', data.text);
   })
 
   });
