bc9dfdda5b6750444ec4e0f1104e2a52162cfa4b
Cody Nicholson <codynicholson96@gmail.com>
2018-05-29 17:46:25 -0500

    Fixed sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,54 +1,15 @@
 import React from 'react';
-import agent from '../agent';
-import Banner from './Home/Banner';
-import { connect } from 'react-redux';
-import company2 from './company2.png';
-import company1 from './company1.png';
-import company3 from './company3.png';
-import company4 from './company4.png';
-import words from './words.png';
+import SponsorsBoard from "./Sponsors/SponsorsBoard";
+import Appp from "./Sponsors/index";
 
-import {
-  SPONSORS_PAGE_LOADED
-} from '../constants/actionTypes';
-
-const mapStateToProps = state => ({
-    appName: state.common.appName
-});
-
-const mapDispatchToProps = dispatch => ({
-  onLoad: payload =>
-    dispatch({ type: SPONSORS_PAGE_LOADED, payload })
-});
-
-export class Sponsors extends React.Component {
-  constructor() {
-    super();
-    //const updateFieldEvent = key => ev => this.props.onUpdateField(key, ev.target.value);
-  }
-
-  render() {
-    return (
-
-
-      <div className="home-page">
-        <Banner token={this.props.token} appName={this.props.appName} declaration="Meet our sponsors!"/>
-
- 
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-10 offset-md-1 col-xs-12">
-              <img width = {300} src={company2} alt="Manoloria" className="centerGraphic"/><br />
-              <img width = {500} src={company1} alt="Manoloria" className="centerGraphic"/><br />
-              <img width = {300} src={company3} alt="Manoloria" className="centerGraphic"/><br />
-              <img width = {300} src={company4} alt="Manoloria" className="centerGraphic"/><br />
-              <img width = {300} src={words} alt="Manoloria" className="centerGraphic"/><br />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+class Sponsors extends React.Component{
+	render(){
+		return(
+				<div>
+					<Appp />
+				</div>
+			);
+	}
 }
 
 export default Sponsors;
