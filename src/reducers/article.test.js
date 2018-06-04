8a62a9a7826ccdf17bf2993f82e5d759213dd65b
Carissa Ward <CarissaNWard@gmail.com>
2018-06-03 23:45:54 -0500

    article.test.js completed

--- src/reducers/article.test.js
+++ src/reducers/article.test.js
@@ -10,14 +10,13 @@ describe('article reducer', () => {
 
   it('should handle ARTICLE_PAGE_LOADED', () => {
     expect(
-      article(["test1","test2"], {
+      article(["state"], {
         type: types.ARTICLE_PAGE_LOADED,
         payload: ["test1", "test1"]
       })
     ).toEqual(
  		{
- 			"0": "test1",
- 			"1": "test2",
+ 			"0": "state",
  			"article": undefined,
  			"comments": undefined
  		}
@@ -27,14 +26,62 @@ describe('article reducer', () => {
 	var commentsPayload = {comments:"relatable; nostalgia; love this song;"};
 
     expect(
-      article(["test1","test2"], {
+      article(["state"], {
         type: types.ARTICLE_PAGE_LOADED,
         payload: [articlePayload, commentsPayload]
       })
     ).toEqual(
-{"0": "test1", "1": "test2", "article": "This is a story all about how my life got flip turned upside down...", "comments": "relatable; nostalgia; love this song;"}
-    )
+		{
+			"0": "state",
+			"article": "This is a story all about how my life got flip turned upside down...", 
+			"comments": "relatable; nostalgia; love this song;"
+		})
+  })
 
+
+  it('should handle ARTICLE_PAGE_UNLOADED', () => {
+    expect(
+      article([], {
+        type: types.ARTICLE_PAGE_UNLOADED
+      })
+    ).toEqual({})
+  })
+
+
+
+  it('should handle ADD_COMMENT', () => {
+
+  	var commentsData = ["commentA", "commentB", "commentC"];
+  	expect(article({comments: commentsData}, 
+  	{
+  		type: types.ADD_COMMENT, 
+  		payload: {comment:"commentD"}
+  	})
+  	).toEqual(
+		{
+			"commentErrors": null, 
+			"comments": ["commentA", "commentB", "commentC", "commentD"]
+		}
+	)
+  })
+
+  it('should handle DELETE_COMMENT', () => {
+
+  	var commentsData = [{id:4, text:"commentA"}, {id:8, text:"commentB"}, {id:2, text:"commentC"}];
+
+  	expect(article({comments: commentsData}, 
+  	{
+  		type: types.DELETE_COMMENT, 
+  		commentId: 8
+  	})
+  	).toEqual(
+		{
+			"comments": [
+				{"id": 4, "text": "commentA"}, 
+				{"id": 2, "text": "commentC"}
+			]
+		}
+	)
   })
 
 })
